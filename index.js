d5d39fa03dcfed7816d4161de0fc7996c273ce51
Sam Comstock <31906344+s-comstock@users.noreply.github.com>
2026-09-14 10:54:57 -0400

    Updating index

--- index.js
+++ index.js
@@ -2,44 +2,38 @@
 Flux Marine Custom JavaScript Compile
 --------------------*/
 
-slater_import('/project/20739/page/63312.js') // Import accordion-card.js)
+/* Register GSAP Plugins */
+gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase)
+
+/* Import components and functions */
+slater_import('/project/20739/page/63312.js') // Import accordion-card.js
 slater_import('/project/20739/page/63318.js') // Import splide-swiper.js
 slater_import('/project/20739/page/63564.js') // Import bunny-player-simple.js
-slater_import('/project/20739/page/63265.js') // Import bunny-background.js
-slater_import('/project/20739/page/63680.js') // Import metric-converter.js')
-slater_import('/project/20739/page/43742.js') // Import lenis-smooth-scroll.js
+slater_import('/project/20739/page/63266.js') // Import bunny-background.js
+slater_import('/project/20739/page/63680.js') // Import metric-converter.js
+slater_import('/project/20739/page/63910.js') // Import lenis-smooth-scroll.js
 slater_import('/project/20739/page/63533.js') // Import modal.js
 slater_import('/project/20739/page/63568.js') // Import tab-scroll.js
 slater_import('/project/20739/page/63270.js') // Import nav.js
 slater_import('/project/20739/page/63506.js') // Import scroll-text.js
+slater_import('/project/20739/page/64092.js') // Import accordion.js
+slater_import('/project/20739/page/64120.js') // Import range-finder.js
 
-/* Webflow Starter */
+/* Initialize after Webflow finishes loading */
 window.Webflow ||= [];
 window.Webflow.push(() => {
 
-  accordionCardInit();
-  splideSwiperInit();
-  initBunnyPlayerBasic();
-  initBunnyPlayerBackground();
-  initMetricConverter();
-  initModal();
-  initTabScrollReveal();
-  initNav();
-  initScrollText();
-  initSmoothScroll();
+  initAccordionCard()
+  initAccordion()
+  initSplideSwiper()
+  initBunnyPlayerBasic()
+  initBunnyPlayerBackground()
+  initMetricConverter()
+  initModal()
+  initTabScrollReveal()
+  initNav()
+  initScrollText()
+  initSmoothScroll()
+  initRangeFinder();
 
 })
-
-/*
-if ('ResizeObserver' in window) {
-  let resizeTimeout;
-  const resizeObserver = new ResizeObserver(() => {
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(() => {
-      lenis.resize();
-      ScrollTrigger.refresh();
-    }, 150);
-  });
-  resizeObserver.observe(document.body);
-}
-*/
